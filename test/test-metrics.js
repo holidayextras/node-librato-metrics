8eab053131fb38984d74092a37c0a73a2b07fe58
Felix Geisendörfer <felix@debuggable.com>
2012-02-08 11:29:51 +0000

    Check in initial implementation

---
+++ test/test-metrics.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var client = require('..').createClient({
+  email: process.env.LIBRATO_METRICS_EMAIL,
+  token: process.env.LIBRATO_METRICS_TOKEN,
+});
+
+
+var json = {
+  gauges: [
+    {name: 'node-librato-metrics-test', value: 123},
+  ],
+};
+
+client.post('/metrics', json, function(err, response) {
+  if (err) throw err;
+
+  var metricName = json.gauges[0].name;
+  client.get('/metrics/' + metricName, function(err, response) {
+    if (err) throw err;
+
+    assert.equal(response.name, metricName);
+  });
+});
