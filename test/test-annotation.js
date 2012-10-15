f7c697f5d134ccdaa2c2b5b0d35a6826e30c02ff
Remy Loubradou <remyloubradou@gmail.com>
2012-10-15 17:59:01 +0100

    Add failing test, response body parsed twice.

---
+++ test/test-annotation.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var client = require('..').createClient({
+  email: process.env.LIBRATO_METRICS_EMAIL,
+  token: process.env.LIBRATO_METRICS_TOKEN,
+});
+
+var annotationName = 'node-librato-metrics-annotation-test';
+var annotationEvent = {
+  title : 'annotation-test',
+  source : 'node-librato-metrics-test',
+  start_time : Date.now() / 1000
+};
+client.post('/annotations/' + annotationName,annotationEvent,function(err,data){
+  if(err) throw err;
+  assert.equal(data.title,annotationEvent.title);
+  assert.equal(data.source,annotationEvent.source);
+  assert.equal(data.end_time,null);
+  assert.equal(data.description,null);
+});
